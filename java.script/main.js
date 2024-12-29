4d8cb240b3affb514ba441b81bb257f948732c57
yaqootgharaibeh <yagoutgharaibeh@gmail.com>
2024-12-29 23:35:42 +0300

    :java-script

---
+++ java.script/main.js
@@ -0,0 +1,49 @@
+{var a,b,n1,n2,n3,price,discount;
+a=1000;
+b=500;
+n1=7;
+n2=9;
+n3=2;
+price=150;
+discount=30/100;
+}
+document.writeln(a/b +"<br>")
+document.writeln( a-b+"<br>")
+document.writeln(a+b+"<br>")
+document.writeln(a*b+"<br>")
+document.writeln( (n1+n2+n3)/3+"<br>")
+document.writeln(price-(price*discount)+"<br>")
+
+var number=20;
+    if(18< number <30){
+        document.writeln(true)
+    }
+{var x,y;
+ x=2;
+y=3;}
+document.writeln(x**y+"<br>")
+{var x,y;
+    x=10;
+   y=4;}
+   document.writeln(x%y+"<br>")
+
+   document.writeln(typeof(100)+"<br>")
+   document.writeln(typeof(73.9)+"<br>")
+   document.writeln(typeof(NaN)+"<br>")
+   document.writeln(typeof("water")+"<br>")
+   document.writeln(typeof(false)+"<br>")
+   document.writeln(typeof(9 != 11)+"<br>")
+   document.writeln("Orang" + "e" +"<br>")
+   document.writeln("Orange" - "s"+"<br>")
+   document.writeln("4" + "8" +"<br>")
+   document.writeln("4" - "8" +"<br>")
+   document.writeln("name" + 3+"<br>" )
+   document.writeln("name" - 3 +"<br>")
+   document.writeln(82 * "word"+"<br>" )
+   document.writeln(1 + "hello"+"<br>")
+   document.writeln("hello" + 1+"<br>" )
+   document.writeln(1 + true +"<br>")
+   document.writeln("hello" + true +"<br>")
+   document.writeln(typeof (Infinity)+"<br>" )
+   document.writeln(1 == '1'+"<br>")
+   document.writeln(1 === '1'+"<br>")
